aea4fcbcc5f943885e3f64a788ca024ee049cd8c
EgorPoprotskiy <EgorPoprotskiy@gmail.com>
2023-06-02 16:49:46 +1000

    using Handler and looper

--- app/src/main/java/com/egorpoprotskiy/corutinestart/MainActivity.kt
+++ app/src/main/java/com/egorpoprotskiy/corutinestart/MainActivity.kt
@@ -6,9 +6,13 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import android.util.Log
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.egorpoprotskiy.corutinestart.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -16,62 +20,91 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    //создание handler в главном потоке(устаревшая интерпретация)
-//    private val handler = Handler()
-    //Обработка сообщений в Handler(для примера)
-//    private val handler = object : Handler() {
-//        override fun handleMessage(msg: Message) {
-//            super.handleMessage(msg)
-//            println("HANDKE_MSG $msg")
-//        }
-//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.buttonLoad.setOnClickListener {
-            loadData()
+//            lifecycleScope.launch {
+//                loadData()
+//            }
+            loadWithoutCoroutines()
         }
-        //Обработка сообщений в Handler(для примера)
-//        handler.sendMessage(Message.obtain(handler, 0, 17))
     }
 
-    private fun loadData() {
+    private suspend fun loadData() {
+        Log.d("MainActivity", "Load started: $this")
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
-        loadCity {
-            binding.tvLocation.text = it
-            loadTemperature(it) {
-                binding.tvTrmperature.text = it.toString()
+        val city = loadCity()
+
+        binding.tvLocation.text = city
+        val temp = loadTemperature(city)
+
+        binding.tvTrmperature.text = temp.toString()
+        binding.progress.isVisible = false
+        binding.buttonLoad.isEnabled = true
+        Log.d("MainActivity", "Load finished: $this")
+    }
+
+    private fun loadWithoutCoroutines(step: Int = 0, obj: Any? = null) {
+        when (step) {
+            0 -> {
+                Log.d("MainActivity", "Load started: $this")
+                binding.progress.isVisible = true
+                binding.buttonLoad.isEnabled = false
+                loadCityWithoutCoroutines {
+                    loadWithoutCoroutines(1, it)
+                }
+            }
+            1 -> {
+                val city = obj as String
+                binding.tvLocation.text = city
+                loadTemperatureWithoutCoroutines(city) {
+                    loadWithoutCoroutines(2, it)
+                }
+            }
+            2 -> {
+                val temp = obj as Int
+                binding.tvTrmperature.text = temp.toString()
                 binding.progress.isVisible = false
                 binding.buttonLoad.isEnabled = true
+                Log.d("MainActivity", "Load finished: $this")
             }
         }
     }
+
+    private fun loadCityWithoutCoroutines(callback: (String) -> Unit) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback.invoke("Moscow")
+        }, 5000)
+    }
+
     //callback создается для возврата значений(в данном случае нам надо вернуть строку)
-    private fun loadCity(callback: (String) -> Unit) {
-        thread {
-            Thread.sleep(5000)
-            //2.4 Еще более новая версия вызова Handle-а из главного потока
-            runOnUiThread {
-                callback.invoke("Moscow")
-            }
-        }
+    private suspend fun loadCity(): String {
+        delay(5000)
+        return "Moscow"
     }
+
+    private fun loadTemperatureWithoutCoroutines(city: String, callback: (Int) -> Unit) {
+        Toast.makeText(
+            this,
+            getString(R.string.load_temperature_toast, city),
+            Toast.LENGTH_SHORT
+        ).show()
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback.invoke(17)
+        }, 5000)
+    }
+
     //callback создается для возврата значений(в данном случае нам надо вернуть число)
-    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
-        thread {
-            //2.4 Еще более новая версия вызова Handle-а из главного потока
-            runOnUiThread {
-                Toast.makeText(
-                    this,
-                    getString(R.string.load_temperature_toast, city),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            Thread.sleep(5000)
-            runOnUiThread {
-                callback.invoke(17)
-            }
-        }
+    private suspend fun loadTemperature(city: String): Int {
+        Toast.makeText(
+            this,
+            getString(R.string.load_temperature_toast, city),
+            Toast.LENGTH_SHORT
+        ).show()
+        delay(5000)
+        return 17
     }
 }
